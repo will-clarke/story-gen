408df6634c8123c7cf9789d48f87ce7ebd45d6c5
Will Clarke <will.clarke@justeattakeaway.com>
2023-08-11 15:39:47 +0100

    WIP

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from langchain.llms import CTransformers
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 
-model_name = "TheBloke/Llama-2-7B-Chat-GGML"
+# model_name = "TheBloke/Llama-2-7B-Chat-GGML"
+model_name = "./models/llama-2-7b-chat-ggml.bin"
 
 config = {'max_new_tokens': 256, 'repetition_penalty': 1.1}
 
-llm = CTransformers(model=model_name, config=config)
+llm = CTransformers(model=model_name, model_type="llama", config=config, client=None)
 
-print(llm('AI is going to'))
+prompt = PromptTemplate.from_template("What is a good name for a company that makes {product}?")
+prompt.format(product="colorful socks")
+
+
+chain = LLMChain(
+    llm=llm,
+    prompt=prompt,
+    # output_parser=CommaSeparatedListOutputParser()
+)
+
+out = chain.run(product="socks")
+
+print(out)
 
