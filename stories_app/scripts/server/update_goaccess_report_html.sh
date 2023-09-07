8bb670c10d71a1a7948ae00909d0aed1a067a6b6
William Clarke <wmmclarke@gmail.com>
2023-09-07 21:51:49 +0100

    add server scripts v2

--- stories_app/scripts/server/update_goaccess_report_html.sh
+++ stories_app/scripts/server/update_goaccess_report_html.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 /usr/bin/zcat -f /var/log/nginx/access.log* | /usr/bin/goaccess --log-format=COMBINED --output /home/will/story-gen/stories_app/static/report.html >> /home/will/cron.log 2>&1
